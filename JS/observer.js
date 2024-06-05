64e87fd5e65dfb74b396e9eb728ade79aad1b575
Pasindu Charith <65920302+Pc-bytn@users.noreply.github.com>
2024-06-05 14:48:54 +0530

    Hero & Testimonial Sections are fixed

--- JS/observer.js
+++ JS/observer.js
@@ -1,19 +1,22 @@
+
 const animation_elements = document.querySelectorAll('.animate-on-scroll');
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
+        // If the entry is intersecting (visible in viewport), add 'animate' class
         if (entry.isIntersecting) {
             entry.target.classList.add('animate');
         } else {
+            // Otherwise, remove 'animate' class
             entry.target.classList.remove('animate');
         }
-    })
-
+    });
 }, {
+    // Set the threshold for triggering the observer callback
     threshold: 0.5
 });
 
-for (let i = 0; i < animation_elements.length; i++) {
-    const el = animation_elements[i];
+// Observe each element in the NodeList
+animation_elements.forEach((el) => {
     observer.observe(el);
-}
+});
